ac88a38dbf0d17e79b99d855a573ed5111be05f7
masteraychen <858665768@qq.com>
2021-01-02 20:25:06 +0800

    good build for aiyunji.love charity project

--- contracts/aiyunji.mall/include/aiyunji.mall/aiyunji.mall.hpp
+++ contracts/aiyunji.mall/include/aiyunji.mall/aiyunji.mall.hpp
@@ -55,3 +55,41 @@ namespace ayj {
    };
 
 }
+
+
+//mall related
+
+
+CONTRACT delife : public contract
+{
+    using contract::contract;
+
+    public:
+        ACTION init();
+        ACTION config(string conf_key, uint64_t conf_val);
+        ACTION add_admin(const name issuer, regid admin, bool is_supper_admin);
+        ACTION del_admin(regid issuer, regid admin);
+        ACTION add_project(regid issuer, string proj_code);
+        ACTION del_project(regid issuer, uint64_t proj_id);
+
+      
+        //mall ACTIONS
+        ACTION credit(regid issuer, regid consumer, uint64_t consume_points);
+        ACTION decredit(regid issuer, regid consumer, uint64_t consume_points);
+        ACTION withdraw(regid issuer, regid to, asset quantity);
+        ACTION invest(regid issuer, regid investor, uint64_t invest_points);
+        ACTION emit_reward();
+
+        ACTION showid(uint64_t id);
+
+        void    pre_action();
+
+    private:
+        uint64_t    gen_new_id(const wasm::name &counter_key, bool persist = true);
+        uint64_t    inc_admin_counter(bool increase_by_one = true);
+        void        check_admin_auth(regid issuer, bool need_super_admin = false);
+        void        set_config(string conf_key, uint64_t conf_val);
+        uint64_t    get_config(string conf_key);
+
+
+}; //contract delife
